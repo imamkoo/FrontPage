901ad11470a9e2d38d02a8300e8be23d434306bc
imamkoo <imammm.kooo@gmail.com>
2022-10-31 01:46:29 +0700

    [imm] inital commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const withCss = require("@zeit/next-css");
+const withReactSvg = require("next-react-svg");
+const withImages = require("next-images");
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    withCss({}),
+    withImages({}),
+    withReactSvg({
+      include: path.resolve(__dirname, "./public/images"),
+      webpack(config, options) {
+        return config;
+      },
+    }),
+  ],
+  nextConfig
+);
